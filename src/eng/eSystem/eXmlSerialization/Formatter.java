997d7267614b0f877bf44eb6c4c9bd548deb1bd1
Engin1980 <engin@seznam.cz>
2018-01-24 23:46:51 +0100

    Added some JavaDoc documentation.

---
+++ src/eng/eSystem/eXmlSerialization/Formatter.java
@@ -0,0 +1,4 @@
+package eng.eSystem.eXmlSerialization;
+
+class Formatter {
+}
--- src/eng/eSystem/eXmlSerialization/XmlListItemMapping.java
+++ src/eng/eSystem/eXmlSerialization/XmlListItemMapping.java
@@ -6,17 +6,31 @@
 
 package eng.eSystem.eXmlSerialization;
 
+import com.sun.istack.internal.NotNull;
+import com.sun.istack.internal.Nullable;
+
 /**
- *
+ * Represents mapping from xml-element representing list to list instance.
+ * <p>
+ *   During deserialization, when a mechanism finds a field in the class which is List
+ *   (or one of its descendants), it will create a new list (according to settings in {@linkplain Settings#getDefaultListTypeImplementation()}).
+ *   Then, to fill the list, it will take all sub-elements of the element as an object in the list. As Java is using
+ *   "generic type erasure", during runtime there is not information what the type in the list should be put.
+ *   This mapping defines, that for specific element ({@linkplain #listPathRegex}) when used as list, (and optionally
+ *   for specific sub-element in the list named to {@linkplain #itemPathRegexOrNull}) the instance of class
+ *   {@linkplain #itemType} will be used.
+ * </p>
  * @author Marek
+ * @see Settings
  */
-class XmlListItemMapping {
+public class XmlListItemMapping {
   /**
    * Represents an XML path to list.
    * Note that this is a regex, xml sub-elements are divided by a dot ".".
    * This should contain either full name to element name containing list, like "^person.addresses" or
    * just regex like "person.addresses".
    */
+  @NotNull
   public final String listPathRegex;
   /**
    * Represents element name inside list if requested.
@@ -26,19 +40,38 @@ class XmlListItemMapping {
    * If specified, only elements inside list {@linkplain listPathRegex} with this tag will be mapped to
    * {@linkplain itemType}.
    */
+  @Nullable
   public final String itemPathRegexOrNull;
   /**
    * Target class to which the element should be mapped into.
    */
+  @NotNull
   public final Class itemType;
 
-  public XmlListItemMapping(String listPathRegex, String itemPathRegexOrNull, Class itemType) {
+  /**
+   * Creates new list-mapping between xml-element and list in java class.
+   * <p>For principle look at the {@link XmlListItemMapping class definition}.
+   * This constructor is used specific class should be used for specific xml-element (according to its name)
+   * in the list xml-element.</p>
+   * @param listPathRegex The regex defining the name of xml-element which contains list items.
+   * @param itemPathRegexOrNull The regex defining the name of xml-subelement inside the list xml-element.
+   * @param itemType The class used to represent item in the list.
+   */
+  public XmlListItemMapping(@NotNull String listPathRegex, @Nullable String itemPathRegexOrNull, @NotNull Class itemType) {
     this.listPathRegex = listPathRegex;
     this.itemPathRegexOrNull = itemPathRegexOrNull;
     this.itemType = itemType;
   }
 
-  public XmlListItemMapping(String listPathRegex, Class itemType) {
+  /**
+   * Creates new list-mapping between xml-element and list in java class.
+   * <p>For principle look at the {@link XmlListItemMapping class definition}.
+   * This constructor is used when all elements in the list has the same
+   * instance type.</p>
+   * @param listPathRegex The regex defining the name of xml-element which contains list items.
+   * @param itemType The class used to represent item in the list.
+   */
+  public XmlListItemMapping(@NotNull String listPathRegex, @NotNull Class itemType) {
     this.listPathRegex = listPathRegex;
     this.itemPathRegexOrNull = null;
     this.itemType = itemType;
