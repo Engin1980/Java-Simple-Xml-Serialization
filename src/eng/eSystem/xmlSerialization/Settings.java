a27f7892f1359a58aa5105db40c7da83dd595f43
Engin1980 <engin@seznam.cz>
2018-03-08 16:01:01 +0100

    UPD: Custom field mappings suppressed.

--- src/eng/eSystem/xmlSerialization/Settings.java
+++ src/eng/eSystem/xmlSerialization/Settings.java
@@ -15,7 +15,7 @@ public class Settings {
   private final List<IValueParser> valueParsers = new ArrayList<>();
   private final List<IElementParser> elementParsers = new ArrayList<>();
   private final List<XmlListItemMapping> listItemMapping = new ArrayList();
-  private final List<XmlCustomFieldMapping> customFieldMappings = new ArrayList<>();
+  //private final List<XmlCustomFieldMapping> customFieldMappings = new ArrayList<>();
   private boolean verbose = false;
   private String nullString = "(null)";
   private Class defaultListTypeImplementation = ArrayList.class;
@@ -166,14 +166,14 @@ public class Settings {
   }
 
   /**
-   * Gets a list of definitions of custom mappings between element-field based on type.<br />
-   * This is mainly used for inheritance. If a instance of a specific class should be used instead of parent class, this mapping must be
-   * defined in this list. For <i>abstract</i> classes it is required as it is not possible to create
-   * an instance of abstract class. How element is defined see {@linkplain XmlCustomFieldMapping}.
-   * @return
-   * @see XmlCustomFieldMapping
-   */
-  public List<XmlCustomFieldMapping> getCustomFieldMappings() {
-    return customFieldMappings;
-  }
+//   * Gets a list of definitions of custom mappings between element-field based on type.<br />
+//   * This is mainly used for inheritance. If a instance of a specific class should be used instead of parent class, this mapping must be
+//   * defined in this list. For <i>abstract</i> classes it is required as it is not possible to create
+//   * an instance of abstract class. How element is defined see {@linkplain XmlCustomFieldMapping}.
+//   * @return
+//   * @see XmlCustomFieldMapping
+//   */
+//  public List<XmlCustomFieldMapping> getCustomFieldMappings() {
+//    return customFieldMappings;
+//  }
 }
--- src/eng/eSystem/xmlSerialization/XmlDeserializationException.java
+++ src/eng/eSystem/xmlSerialization/XmlDeserializationException.java
@@ -1,7 +1,5 @@
 package eng.eSystem.xmlSerialization;
 
-import sun.rmi.transport.ObjectTable;
-
 public class XmlDeserializationException extends InternalXmlException {
   public XmlDeserializationException(String message, Object ... params) {
     super(String.format(message, params));
